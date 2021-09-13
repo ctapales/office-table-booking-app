1a287699f3b7915e3e0563c8f64f1c65c1f93f2f
sciu-ctapales <carl@sciunitedllp.com>
2021-09-13 20:48:29 +0800

    Added functional components

---
+++ react-app/src/pages/Home/Home1.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import { Container } from "react-bootstrap";
+
+export default function Home1(){
+    const [schedule, setSchedule] = useState(new Date());
+
+    return(
+        <div className="page-home">
+        <Container>
+         
+        </Container>
+      </div>  
+    )
+}
